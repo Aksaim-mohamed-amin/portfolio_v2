1ab98bc59f971479e2d4e6581cc64177eccfb8d3
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2025-04-21 18:13:34 +0100

    Mobilde responice

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,40 +1,54 @@
-import {Component, HostListener} from '@angular/core';
+import {AfterViewInit, Component, HostListener} from '@angular/core';
+
+declare const bootstrap: any;
 
 @Component({
-    selector: 'app-header',
-    imports: [],
-    templateUrl: './header.component.html',
-    styleUrl: './header.component.css'
+	selector: 'app-header',
+	imports: [],
+	templateUrl: './header.component.html',
+	styleUrl: './header.component.css'
 })
-export class HeaderComponent {
-    activeSection: string = 'home';
+export class HeaderComponent implements AfterViewInit {
+	activeSection: string = 'home';
 
-    constructor() {
-        this.onScroll();
-    }
+	constructor() {
+		this.onScroll();
+	}
 
-    @HostListener('window:scroll', [])
-    onScroll() {
-        const sections: string[] = ['home', 'skills', 'projects', 'contact'];
-        const header = document.querySelector('header');
+	ngAfterViewInit(): void {
+		document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
+			link.addEventListener('click', () => {
+				const navbar = document.getElementById('navbarSupportedContent');
+				const bsCollapse = new bootstrap.Collapse(navbar, {
+					toggle: false
+				});
+				bsCollapse.hide();
+			});
+		});
+	}
+
+	@HostListener('window:scroll', [])
+	onScroll() {
+		const sections: string[] = [ 'home', 'skills', 'projects', 'contact' ];
+		const header = document.querySelector('header');
 
 
-        if (window.scrollY > 40) {
-            header?.classList.add('scrolled');
-        } else {
-            header?.classList.remove('scrolled');
-        }
+		if (window.scrollY > 40) {
+			header?.classList.add('scrolled');
+		} else {
+			header?.classList.remove('scrolled');
+		}
 
-        for (let section of sections) {
-            const element = document.getElementById(section);
-            if (element) {
-                const rect = element.getBoundingClientRect();
-                const middle = window.innerHeight / 2;
-                if (rect.top <= middle && rect.bottom >= middle) {
-                    this.activeSection = section;
-                    break;
-                }
-            }
-        }
-    }
+		for (let section of sections) {
+			const element = document.getElementById(section);
+			if (element) {
+				const rect = element.getBoundingClientRect();
+				const middle = window.innerHeight / 2;
+				if (rect.top <= middle && rect.bottom >= middle) {
+					this.activeSection = section;
+					break;
+				}
+			}
+		}
+	}
 }
